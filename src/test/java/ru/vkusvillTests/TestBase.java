55609597c94071bb5bd81446d9f2cc4c479874f4
Oleg Borzunov <olezhoncsgo@gmail.com>
2025-04-08 11:29:24 +0500

    added configuration

---
+++ src/test/java/ru/vkusvillTests/TestBase.java
@@ -0,0 +1,24 @@
+package ru.vkusvillTests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    @BeforeAll
+    static void configuration() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "normal";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 12000;
+        Configuration.pollingInterval = 500;
+    }
+
+    @AfterEach
+    void tearDown() {
+        Selenide.closeWindow();
+    }
+}
